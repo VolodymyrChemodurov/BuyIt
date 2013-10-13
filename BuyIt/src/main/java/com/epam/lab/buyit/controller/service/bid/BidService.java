0fbb9f230f81761d37f1097ed3b408ed374adcab
mr.Rand <mr.Rand@Andrii>
2013-10-13 14:21:15 +0300

    + Bid service

---
+++ BuyIt/src/main/java/com/epam/lab/buyit/controller/service/bid/BidService.java
@@ -0,0 +1,8 @@
+package com.epam.lab.buyit.controller.service.bid;
+
+import com.epam.lab.buyit.controller.service.GenericService;
+import com.epam.lab.buyit.model.Bid;
+
+public interface BidService extends GenericService<Bid> {
+
+}
