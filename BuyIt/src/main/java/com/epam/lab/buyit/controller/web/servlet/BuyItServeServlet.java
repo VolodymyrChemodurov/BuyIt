d6aa5e8acbfddcd68f4d8d5da8589988fdf8c026
Chemodurov Volodymyr <chemodurov92@gmail.com>
2013-10-20 00:24:14 +0300

    BuyItServeServlet refactoring

--- BuyIt/src/main/java/com/epam/lab/buyit/controller/web/servlet/BuyItServeServlet.java
+++ BuyIt/src/main/java/com/epam/lab/buyit/controller/web/servlet/BuyItServeServlet.java
@@ -18,6 +18,15 @@ import com.epam.lab.buyit.model.User;
 
 public class BuyItServeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private AuctionServiceImp auctionService;
+	private BidServiceImp bidService;
+	private ProductService productService;
+
+	public void init() {
+		auctionService = new AuctionServiceImp();
+		bidService = new BidServiceImp();
+		productService = new ProductServiceImpl();
+	}
 
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
@@ -31,19 +40,18 @@ public class BuyItServeServlet extends HttpServlet {
 
 	private void serve(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
+
 		int idProduct = Integer.parseInt(request.getParameter("id_product"));
 		int count = Integer.parseInt(request.getParameter("quantity"));
 		User user = (User) request.getSession(false).getAttribute("user");
 
-		AuctionServiceImp auctionService = new AuctionServiceImp();
-		BidServiceImp bidService = new BidServiceImp();
 		Auction auction = auctionService.getByProductId(idProduct);
 		int realCount = auction.getCount();
 		String status = null;
 
 		if (auction.getStatus().equals("inProgress")) {
 			if (realCount < count) {
-				response.sendRedirect("cfxgc");
+				wrongProductCount(request, response);
 			} else if (realCount - count == 0) {
 				status = "closed";
 			} else {
@@ -53,29 +61,50 @@ public class BuyItServeServlet extends HttpServlet {
 					auction.getIdAuction(), realCount - count, status,
 					realCount, auction.getStatus());
 			if (affectedRows == 1) {
-				Bid bid = new Bid();
-				bid.setTime(new Timestamp(System.currentTimeMillis()));
-				bid.setAmount(auction.getBuyItNow());
-				bid.setAuctionId(auction.getIdAuction());
-				bid.setUserId(user.getIdUser());
-				bidService.createItem(bid);
-				
-				ProductService productService = new ProductServiceImpl();
-				request.setAttribute("product", productService.getItemById(idProduct));
-				request.setAttribute("user", user);
-				request.setAttribute("bid", bid);
-				request.setAttribute("count", count);
-				request.getRequestDispatcher("deal_information").forward(request, response);
+				successfulPurchase(request, response, auction, user, realCount, idProduct);
 			} else {
-				request.setAttribute("message", "Sorry, some error with query.");
-				request.setAttribute("alert", "error");
-				request.getRequestDispatcher("message_page").forward(request, response);
+				queryFail(request, response);
 			}
-
 		} else {
-			request.setAttribute("message", "Sorry, this auction allready closed.");
-			request.setAttribute("alert", "block");
-			request.getRequestDispatcher("message_page").forward(request, response);
+			auctionAllreadyClosed(request, response);
 		}
+	}
+
+	private void successfulPurchase(HttpServletRequest request,
+			HttpServletResponse response, Auction auction, User user,
+			int count, int idProduct) throws ServletException, IOException {
+		Bid bid = new Bid();
+		bid.setTime(new Timestamp(System.currentTimeMillis()));
+		bid.setAmount(auction.getBuyItNow());
+		bid.setAuctionId(auction.getIdAuction());
+		bid.setUserId(user.getIdUser());
+		bidService.createItem(bid);
+
+		request.setAttribute("product", productService.getItemById(idProduct));
+		request.setAttribute("user", user);
+		request.setAttribute("bid", bid);
+		request.setAttribute("count", count);
+		request.getRequestDispatcher("deal_information").forward(request,response);
+	}
+
+	private void queryFail(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("message", "Sorry, some error with query.");
+		request.setAttribute("alert", "error");
+		request.getRequestDispatcher("message_page").forward(request, response);
+	}
+
+	private void auctionAllreadyClosed(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("message", "Sorry, this auction allready closed.");
+		request.setAttribute("alert", "block");
+		request.getRequestDispatcher("message_page").forward(request, response);
+	}
+
+	private void wrongProductCount(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("message","Sorry, you count exceeds the existing.");
+		request.setAttribute("alert", "block");
+		request.getRequestDispatcher("message_page").forward(request, response);
 	}
 }
