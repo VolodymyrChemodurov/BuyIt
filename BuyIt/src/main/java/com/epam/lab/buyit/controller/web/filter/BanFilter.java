f20ede32e5217cb51e799842af84bebdd12d6be8
Chemodurov Volodymyr <chemodurov92@gmail.com>
2013-10-22 02:00:32 +0300

    Add BanFilter, update LoginServlet

---
+++ BuyIt/src/main/java/com/epam/lab/buyit/controller/web/filter/BanFilter.java
@@ -0,0 +1,42 @@
+package com.epam.lab.buyit.controller.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.epam.lab.buyit.model.Status;
+import com.epam.lab.buyit.model.User;
+
+public class BanFilter implements Filter {
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+		User user = (User) httpRequest.getSession(false).getAttribute("user");
+		if (user.getBan().equals(Status.BANNED.getStatus())) {
+			httpRequest	.setAttribute("message",
+							"Sorry, you are banned. Write email to administrator for more information");
+			httpRequest.setAttribute("alert", "error");
+			httpRequest.getRequestDispatcher("message_page").forward(
+					httpRequest, httpResponse);
+		} else
+			chain.doFilter(request, response);
+
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
