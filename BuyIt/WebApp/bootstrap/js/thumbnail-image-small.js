dad3d720cbc9b67a490e79f75cb39613bae33124
Chemodurov Volodymyr <chemodurov92@gmail.com>
2013-10-30 11:52:40 +0200

    Update thumbnail-image-small.js

--- BuyIt/WebApp/bootstrap/js/thumbnail-image-small.js
+++ BuyIt/WebApp/bootstrap/js/thumbnail-image-small.js
@@ -1,41 +1,44 @@
 $(window).load(function() {
-        $('.my-image').each(function() {
-                var maxWidth = 200;
-                var maxHeight = 160;
-                var ratio = 0;
-                var width = $(this).width();
-                var height = $(this).height();
-                var flag = false;
-                console.log(this);
-                console.log(width);
-                console.log(height);
-                console.log(flag);
-                if (width > maxWidth) {
-                        ratio = maxWidth / width;
-                        $(this).css("width", maxWidth);
-                        $(this).css("height", height * ratio);
-                        height = height * ratio;
-                        width = width * ratio;
-                        flag = true;
-                }
+	var maxWidth = 200;
+	var maxHeight = 160;
+	var ratio = 0;
+	
+	$('.my-image').each(function() {
+		
+		var width = $(this).width();
+		var height = $(this).height();
+		var flag = false;
+		
+		console.log(this);
+		console.log(width);
+		console.log(height);
+		
+		if (width > maxWidth) {
+			ratio = maxWidth / width;
+			$(this).css("width", maxWidth);
+			$(this).css("height", height * ratio);
+			
+			height = height * ratio;
+			width = width * ratio;
+			flag = true;
+		}
 
-                if (height > maxHeight) {
-                        ratio = maxHeight / height;
-                        $(this).css("height", maxHeight);
-                        $(this).css("width", width * ratio);
-                        width = width * ratio;
-                        height = height * ratio;
-                        flag = true;
-                }
-                console.log(this);
-                console.log(width);
-                console.log(height);
-                console.log(flag);
-                if(!flag) {
-                	$(this).css('margin-top', 160 - Math.ceil(height));
-                	
-                }
-                $(this).parent().parent().css('visibility','visible');
-                flag = false;
-        });
+		if (height > maxHeight) {
+			ratio = maxHeight / height;
+			$(this).css("height", maxHeight);
+			$(this).css("width", width * ratio);
+			width = width * ratio;
+			height = height * ratio;
+			flag = true;
+		}
+		console.log(this);
+		console.log(width);
+		console.log(height);
+		
+		if (!flag) {
+			$(this).css('margin-top', 160 - Math.ceil(height));
+		}
+		$(this).parent().parent().css('visibility', 'visible');
+		flag = false;
+	});
 });
