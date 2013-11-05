806c4b8b6bef6d839b8a7adf2109fc702e611d44
Chemodurov Volodymyr <chemodurov92@gmail.com>
2013-11-05 22:34:06 +0200

    MD5Encryptor refactoring

--- BuyIt/src/main/java/com/epam/lab/buyit/controller/security/MD5Encryptor.java
+++ BuyIt/src/main/java/com/epam/lab/buyit/controller/security/MD5Encryptor.java
@@ -7,25 +7,22 @@ import org.apache.log4j.Logger;
 
 public class MD5Encryptor {
 	private static final Logger LOGGER = Logger.getLogger(MD5Encryptor.class);
-	private static MessageDigest messageDigest;
 	
-	static {
+	public static String encrypt(String password) {
 		try {
-			messageDigest = MessageDigest.getInstance("MD5");
+			MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+			messageDigest.update(password.getBytes());
+	        byte byteData[] = messageDigest.digest();
+	 
+	        StringBuffer encrypted = new StringBuffer();
+	        for (int i = 0; i < byteData.length; i++) {
+	        	encrypted.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+	        }
+	    	return encrypted.toString();
 		} catch (NoSuchAlgorithmException e) {
 			LOGGER.error(e);
 		}
-	}
-	
-	public synchronized static String encrypt(String password) {
-		messageDigest.update(password.getBytes());
-        byte byteData[] = messageDigest.digest();
- 
-        StringBuffer encrypted = new StringBuffer();
-        for (int i = 0; i < byteData.length; i++) {
-         encrypted.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-        }
-    	return encrypted.toString();
+		return password;
 	}
 
 }
