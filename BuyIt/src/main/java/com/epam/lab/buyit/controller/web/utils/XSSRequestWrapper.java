767fa809594cd9a5e140f5366ccd1336421d9a32
Chemodurov Volodymyr <chemodurov92@gmail.com>
2013-11-10 23:33:38 +0200

    Create XSSFilter

---
+++ BuyIt/src/main/java/com/epam/lab/buyit/controller/web/utils/XSSRequestWrapper.java
@@ -0,0 +1,96 @@
+package com.epam.lab.buyit.controller.web.utils;
+
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class XSSRequestWrapper extends HttpServletRequestWrapper {
+
+	public XSSRequestWrapper(HttpServletRequest servletRequest) {
+		super(servletRequest);
+	}
+
+	@Override
+	public String[] getParameterValues(String parameter) {
+		String[] values = super.getParameterValues(parameter);
+
+		if (values == null) {
+			return null;
+		}
+
+		int count = values.length;
+		String[] encodedValues = new String[count];
+		for (int i = 0; i < count; i++) {
+			encodedValues[i] = stripXSS(values[i]);
+		}
+
+		return encodedValues;
+	}
+
+	@Override
+	public String getParameter(String parameter) {
+		String value = super.getParameter(parameter);
+
+		return stripXSS(value);
+	}
+
+	@Override
+	public String getHeader(String name) {
+		String value = super.getHeader(name);
+		return stripXSS(value);
+	}
+
+	private String stripXSS(String value) {
+		if (value != null) {
+			value = value.replaceAll("", "");
+
+			Pattern scriptPattern = Pattern.compile("<script>(.*?)</script>",
+					Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			scriptPattern = Pattern.compile("</script>",
+					Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			scriptPattern = Pattern.compile("<script(.*?)>",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			scriptPattern = Pattern.compile("eval\\((.*?)\\)",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			scriptPattern = Pattern.compile("expression\\((.*?)\\)",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			scriptPattern = Pattern.compile("javascript:",
+					Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			scriptPattern = Pattern.compile("vbscript:",
+					Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			scriptPattern = Pattern.compile("onload(.*?)=",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+		}
+		return value;
+	}
+}
